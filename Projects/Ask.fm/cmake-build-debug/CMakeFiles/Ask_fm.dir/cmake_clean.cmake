595f027772b5c37da5b26eaecd94d4bea33a69d9
mohamed <mohamdlb506@gmail.com>
2023-09-24 14:50:22 +0300

    init enveroment to build ask.fm project

---
+++ Projects/Ask.fm/cmake-build-debug/CMakeFiles/Ask_fm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Ask_fm.exe"
+  "Ask_fm.exe.manifest"
+  "Ask_fm.pdb"
+  "CMakeFiles/Ask_fm.dir/main.cpp.obj"
+  "libAsk_fm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ask_fm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
